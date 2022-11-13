5773557e9fa1d413b5c0192f302f482e47c9ec6b
Arvin Namdar Ghanbari <arvinnamdarghanbari@Arvins-Air.lan>
2022-11-13 07:34:58 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+print("Hello User")
+
+my_name = "MARIOTASTIC_TECH"
+
+name = input("What is your name??? ")
+
+print("Welcome " + name)
+
+print("My name is " + my_name)
+
+age = input("How old are you? ")
+
+print("so you are " + age)
+
+food = input("What is your favorite food? ")
+
+print("Nice, my favorite food is ketchup with cereal")
+
+weight = input("How much do you weigh(In pounds) ")
+
+weight = input("Weight: ")
+
+
+
+
+
